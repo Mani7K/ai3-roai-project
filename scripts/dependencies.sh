39a0c741bba49e800e0e35ce1f242b7e9459ebb8
Jan Langela Regincos <ai23m057@technikum-wien.at>
2024-12-31 02:51:41 +0100

    updated dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,6 +1,11 @@
 sudo apt update
 sudo apt-get update
-sudo apt install ros-noetic-mir-robot -y
-sudo apt install ros-noetic-navigation -y
-sudo apt install ros-noetic-map-server -y
-sudo apt install ros-noetic-amcl -y
+sudo apt install -y \
+    ros-noetic-mir-robot \
+    ros-noetic-gmapping \
+    ros-noetic-navigation \
+    ros-noetic-map-server \
+    ros-noetic-amcl \
+    ros-noetic-teleop-twist-keyboard \
+    ros-noetic-tf2-tools \
+    ros-noetic-tf2-ros 
